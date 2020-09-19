345ec9043a98289dee2583d3ca025c6093141dd9
Vidhatha Vivekananda <vidhatha.sv@gmail.com>
2020-09-19 19:32:52 +0530

    Create selection_sort.cpp

---
+++ Algorithms/Sorting/SelectionSort/selection_sort.cpp
@@ -0,0 +1,34 @@
+/*
+ * Time complexity = O(n2)
+ * Space complexity = O(n)
+*/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void SelectionSort(vector<int>& a)
+{
+    int min_index;
+    int n = a.size();
+
+    for(auto i=0;i<n-1;++i)
+    {
+        min_index = i;
+        for(auto j =i; j<n;++j)
+        {
+            if(a[j]<a[min_index])
+                min_index = j;
+        }
+        swap(a[i],a[min_index]);
+    }
+}
+
+int main()
+{
+    vector<int> a{12, 8, 3, 7, 10, 8, 23, 5, 17};
+    SelectionSort(a);
+    for(auto x:a)
+        cout<<x<<" ";
+    return 0;
+}
